a7606527e7d98c673d96dea45b34d6f72dccdfa0
YariAn <yari_miranda@hotmail.com>
2017-10-24 21:32:48 -0400

    Used ceiling and floor functions, Introduced error, changed division variable type from const to let

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const a = 3;
 const b = 6;
+const c = 10;
 const sum = a + b;
+let division = c / a;
 
 console.log(`a is equal to ${a}`);
 console.log(`b is equal to ${b}`);
 console.log(`The sum of a and b is equal to ${sum}`);
+
+console.log(`c is equal to ${c}`);
+console.log(`C divided by a is equal to ${division}`);
+console.log(`The ceiling of the division is equal to ${Math.ceil(division)}`);
+console.log(`The floor of the division is equal to ${Math.floor(division)}`);
+
+// Introduce error 
+
+division = 3;
+console.log(`division is equal to ${division}`);
+
